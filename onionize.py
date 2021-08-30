5c990a47dd85b45cafc9d6114f745bd2fb4aef0b
ANISH M <58029804+Anish-M-code@users.noreply.github.com>
2021-08-30 10:43:26 +0530

    Fix to avoid invoking  shell  using os.system()
    Replace os.system() with subprocess.run()

--- onionize.py
+++ onionize.py
@@ -2,6 +2,7 @@ import os
 import time
 import platform
 import sys
+import subprocess
 
 if platform.system().lower() == 'windows':
     print('Platform not supported!')
@@ -9,14 +10,14 @@ if platform.system().lower() == 'windows':
 
 def download_eotk():
     print('Downloading Enterprise Onion Toolkit....\n')
-    os.system(" git clone https://github.com/alecmuffett/eotk.git")
+    x = subprocess.run(['git','clone','https://github.com/alecmuffett/eotk.git'])
     os.chdir('eotk')
 
 def get_domains():
-    domain=[]
+    domain = []
     while 1:
-        x=input('Enter domain of clearnet website:')
-        choice=input('\nDo you want enter more domains?(y/n):')
+        x = input('Enter domain of clearnet website:')
+        choice = input('\nDo you want enter more domains?(y/n):')
         domain.append(x)
         if choice.lower() != 'y':
             break
@@ -32,59 +33,57 @@ while 1:
     print('4) CentOS 8.2.2004 ')
     print('5) macOS Mojave ')
     print('6) FreeBSD 12.1')
-    choice=input('\n\nEnter choice:')
+    choice = input('\n\nEnter choice:')
     print()
 
     if choice == '1':
         print('Installing git...\n')
-        os.system('sudo apt-get install -y git')
+        x = subprocess.run(['sudo','apt-get','install','-y','git'])
         download_eotk()
-        os.system('sudo ./opt.d/build-raspbian-stretch.sh')
+        x = subprocess.run(['sudo','./opt.d/build-raspbian-stretch.sh'])
         break
 
     elif choice == '2':
         download_eotk()
-        os.system('sudo ./opt.d/build-ubuntu-20.04.sh')
+        x = subprocess.run(['sudo','./opt.d/build-ubuntu-20.04.sh'])
         break
 
     elif choice == '3':
         download_eotk()
-        os.system('sudo ./opt.d/build-ubuntu-18.04.sh')
+        x = subprocess.run(['sudo','./opt.d/build-ubuntu-18.04.sh'])
         break
 
     elif choice == '4':
         print('Installing git ...\n')
-        os.system('sudo yum -y install git')
+        x = subprocess.run(['sudo','yum','-y','install','git'])
         download_eotk()
-        os.system('sudo ./opt.d/build-centos-8.2.2004.sh')
+        x = subprocess.run(['sudo','./opt.d/build-centos-8.2.2004.sh'])
         break
 
     elif choice == '5':
         download_eotk()
-        os.system(" ./opt.d/build-macos-mojave.sh")
+        x = subprocess.run(['./opt.d/build-macos-mojave.sh'])
         break
 
     elif choice == '6':
         print('Installing git ...\n')
-        os.system('pkg install git')
+        x = subprocess.run(['pkg','install','git'])
         download_eotk()
-        os.system('./opt.d/build-freebsd-12.1.sh')
+        x = subprocess.run(['./opt.d/build-freebsd-12.1.sh'])
         break
 
     else:
         print("Invalid choice!!!")
 
-domainlist=get_domains()
+domainlist = get_domains()
 with open("tor_server.tconf",'w') as f:
     f.write("set project tor_server\n")
     for domain in domainlist:
         f.write("hardmap %NEW_V3_ONION% "+domain+"\n")
     
-os.system("sudo ./eotk config tor_server.tconf")
-os.system('sudo ./eotk config tor_server.conf')
-os.system("sudo ./eotk start tor_server")
+x = subprocess.run(['sudo','./eotk','config','tor_server.tconf'])
+x = subprocess.run(['sudo','./eotk','config','tor_server.conf'])
+x = subprocess.run(['sudo','./eotk','start','tor_server'])
 print('\nPlease wait ...\n')
 time.sleep(10)
-os.system("sudo ./eotk status tor_server")
-
-
+x = subprocess.run(['sudo','./eotk','status','tor_server'])
